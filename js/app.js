396be9c90f1b1be3ccc24e60f5521b3cdf962bce
Elvan Hidayat <elvanhd@gmail.com>
2023-04-28 10:50:47 +0700

    sidebar functionality

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('#sidebar');
+const date = document.querySelector('#date');
+
+// show sidebar
+navBtn?.addEventListener('click', function () {
+  sidebar?.classList.add('show-sidebar');
+});
+
+closeBtn?.addEventListener('click', function () {
+  sidebar?.classList.remove('show-sidebar');
+});
